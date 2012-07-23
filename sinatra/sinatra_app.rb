4737c317abba70f71242976a86766a8f3e2e3519
Junhyun Shin <hl1sqi@gmail.com>
2012-07-23 17:09:38 +0900

    ...

--- sinatra/sinatra_app.rb
+++ sinatra/sinatra_app.rb
@@ -1,5 +1,26 @@
+
 class SinatraApp < Sinatra::Base
 	get "/" do
-		"Hello, Time is #{Time.now}"
+		erb :test
 	end
+	
+	get "/test" do
+	  erb :test
+	end
+	
+	get "/application.js" do
+	  coffee :coffee
+  end
+
+	get "/coffee_test.js" do
+	  content_type "text/javascript"
+	  coffee :coffee_test
+  end
+
+	get "/json_test.json" do
+	  content_type :json
+    { :key => 'value', :key2 => 'value2' }.to_json
+  end
+  
+  
 end
